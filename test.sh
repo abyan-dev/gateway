08c39dcf062bdc520e2111523e5d3240452fc48d
abyanmajid <abyan@abydyl.net>
2024-07-24 21:46:27 +0700

    refactor: test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,8 @@ mkdir -p reports
 echo "mode: set" > reports/coverage.out
 
 for dir in $(find . -type d ! -path "./cmd/api*"); do
-    ls "$dir"/*_test.go &> /dev/null && go test -coverprofile=tmp_coverage.out "$dir" && tail -n +2 tmp_coverage.out >> reports/coverage.out && rm tmp_coverage.out
+    if ls "$dir"/*_test.go &> /dev/null; then
+        go test -coverprofile=tmp_coverage.out "$dir" && tail -n +2 tmp_coverage.out >> reports/coverage.out
+        rm -f tmp_coverage.out
+    fi
 done
