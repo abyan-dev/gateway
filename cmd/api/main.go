edf60e0f9ffc733c5dc37c407bd3020d84969b6e
abyan <abyan@abydyl.net>
2024-07-24 23:00:22 +0700

    patch: tls and dns errors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,37 +1,27 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
 
 	"github.com/abyan-dev/gateway/pkg/info"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
-	"github.com/valyala/fasthttp"
 )
 
 func main() {
 	app := fiber.New()
 
-	proxy.WithTlsConfig(&tls.Config{
-		InsecureSkipVerify: true,
-	})
-
-	proxy.WithClient(&fasthttp.Client{
-		NoDefaultUserAgentHeader: true,
-		DisablePathNormalizing:   true,
-	})
-
-	// HTTP security middleware
-	app.Use(helmet.New())
-
-	// Logger
 	app.Use(logger.New())
 
-	// Proxies
-	app.Use("/api/auth", proxy.Forward("http://auth/api"))
+	app.All("/api/auth/*", func(c *fiber.Ctx) error {
+		targetURL := "http://auth:8080/api" + c.OriginalURL()[len("/api/auth"):]
+		if err := proxy.Do(c, targetURL); err != nil {
+			return err
+		}
+		c.Response().Header.Del(fiber.HeaderServer)
+		return nil
+	})
 
 	info.IsRunning()
 
